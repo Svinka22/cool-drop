a3573f5de748c2c5d18ac8e55de66a2199e0b6f4
Svinka22 <58908173+Svinka22@users.noreply.github.com>
2020-05-25 13:02:56 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const contauner = document.getElementsByTagName('img')
+contauner[1].addEventListener('click', function() {
+	document.getElementById("myImage-1").src="https://unvegetariano.com/images/star-vector-png-7.png";
+});
+contauner[2].addEventListener('click', function() {
+	document.getElementById("myImage-1").src="https://unvegetariano.com/images/star-vector-png-7.png";
+	document.getElementById("myImage-2").src="https://unvegetariano.com/images/star-vector-png-7.png";
+});
+contauner[3].addEventListener('click', function() {
+	document.getElementById("myImage-1").src="https://unvegetariano.com/images/star-vector-png-7.png";
+	document.getElementById("myImage-2").src="https://unvegetariano.com/images/star-vector-png-7.png";
+	document.getElementById("myImage-3").src="https://unvegetariano.com/images/star-vector-png-7.png";
+});
+contauner[4].addEventListener('click', function() {
+	document.getElementById("myImage-1").src="https://unvegetariano.com/images/star-vector-png-7.png";
+	document.getElementById("myImage-2").src="https://unvegetariano.com/images/star-vector-png-7.png";
+	document.getElementById("myImage-3").src="https://unvegetariano.com/images/star-vector-png-7.png";
+	document.getElementById("myImage-4").src="https://unvegetariano.com/images/star-vector-png-7.png";
+});
+contauner[5].addEventListener('click', function() {
+	document.getElementById("myImage-1").src="https://unvegetariano.com/images/star-vector-png-7.png";
+	document.getElementById("myImage-2").src="https://unvegetariano.com/images/star-vector-png-7.png";
+	document.getElementById("myImage-3").src="https://unvegetariano.com/images/star-vector-png-7.png";
+	document.getElementById("myImage-4").src="https://unvegetariano.com/images/star-vector-png-7.png";
+	document.getElementById("myImage-5").src="https://unvegetariano.com/images/star-vector-png-7.png";
+});
+
